e08cab0cdb195cac4d577392493965afa6152b16
Ken Huang <kenhuangus@users.noreply.github.com>
2022-01-28 13:46:01 -0500

    Update 1.private-key-to-ethereum-address.js

--- demos/1.private-key-to-ethereum-address.js
+++ demos/1.private-key-to-ethereum-address.js
@@ -1,7 +1,7 @@
 let ethers = require('ethers');
 
 let privateKey =
-  '0x012345678901234567890123456789012345678901234567890123456789012';
+  '0x93180051872467db2071fd841caeecac75650c8c7af063630f9568a44d29720f';
 
 let wallet = new ethers.Wallet(privateKey);
 
