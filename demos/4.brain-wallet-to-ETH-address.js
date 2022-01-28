9a00ed0a562284648603b3a19f6f1b1cfa3b2a2c
Ken Huang <kenhuangus@users.noreply.github.com>
2022-01-28 14:02:00 -0500

    Update 4.brain-wallet-to-ETH-address.js

--- demos/4.brain-wallet-to-ETH-address.js
+++ demos/4.brain-wallet-to-ETH-address.js
@@ -3,7 +3,7 @@ let ethers = require('ethers');
 var username = 'support@ethers.io';
 
 var password = 'password123';
-
+//the following is not secure and deprecated. Please do not use
 ethers.Wallet.fromBrainWallet(username, password).then(wallet => {
   console.log('Address: ' + wallet.address);
 });
