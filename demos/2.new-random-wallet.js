24168418304a1d0efe09c800a5bf21ec300a53ae
Ken Huang <kenhuangus@users.noreply.github.com>
2022-02-24 22:21:21 -0500

    Update 2.new-random-wallet.js

--- demos/2.new-random-wallet.js
+++ demos/2.new-random-wallet.js
@@ -1,6 +1,6 @@
 let ethers = require('ethers');
 
 let wallet = ethers.Wallet.createRandom();
-   let wordlist = wallet.mnemonic;
+   let wordlist = wallet.mnemonic.phrase;
     console.log("the mnemonic is  \n"+ wordlist);
 console.log('Address: ' + wallet.address);
